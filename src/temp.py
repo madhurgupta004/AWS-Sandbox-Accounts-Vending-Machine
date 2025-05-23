d7f8d5280e2e86b361195df59a04c234bbfabb98
madhurgupta004 <madhurgupta004@gmail.com>
2025-05-23 15:00:53 +0530

    Update jenkins file

--- src/temp.py
+++
@@ -1,8 +0,0 @@
-from helpers import get_boto3_client, access_key, secret_access_key
-
-from run import get_initial_account_data
-
-client = get_boto3_client('s3' , access_key=access_key, secret_access_key=secret_access_key)
-data = get_initial_account_data()
-print(data)
-print(type(data))
